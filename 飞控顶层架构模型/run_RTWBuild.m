c260215c5acb49be5bbb1e6775d26f1866ad88f1
sl <sl1986-2-23@163.com>
2020-08-13 12:57:14 +0800

    v2.068 基础信息—————————————————— 基础调用周期: 12ms 模块功能: 组合导航 任务管理 信号完好性检测 飞行性能 传感器故障模拟 最大航点500 修改信息—————————————————— 1)  地面模式高度源改为雷达高； 2）增加单任务模式运行控制变量； 3）增加版本信息变量OUT_SYSTEMINFO；

--- 飞控顶层架构模型/run_RTWBuild.m
+++ 飞控顶层架构模型/run_RTWBuild.m
@@ -1,15 +1,34 @@
 modelname = 'RefModel_SystemArchitecture';
+infofilename = 'SystemInfo.mat';
+%% 载入模型
+load_system(modelname);
+open_system([modelname,'/SystemInfo/']);
+%%
+try
+    load(infofilename);
+    SystemInfo.version = num2str(str2num(SystemInfo.version) + 0.001);
+catch
+    SystemInfo.version = '';
+    SystemInfo.date = '';
+end
+tempTime = clock;
+SystemInfo.date = [ num2str(tempTime(1)),...年
+                    num2str(tempTime(2)),...月
+                    num2str(tempTime(3));];%日
 prompt = {'版本号','时间'};
 dlgtitle = '系统信息';
 dims = [1 55];
-tempTime = clock;
-tempTimeStr = [num2str(tempTime(1)),...年
-               num2str(tempTime(2)),...月
-               num2str(tempTime(3));];%日
-definput = {'',tempTimeStr};
+
+definput = {SystemInfo.version,SystemInfo.date};
 answer = inputdlg(prompt,dlgtitle,dims,definput);
-systemInfo.version = answer{1};
-systemInfo.date = answer{2};
-set_param([modelname,'/SystemInfo/version'],'value',systemInfo.version)
-set_param([modelname,'/SystemInfo/date'],'value',systemInfo.date)
+%%
+SystemInfo.version = answer{1};
+SystemInfo.date = answer{2};
+set_param([modelname,'/SystemInfo/version'],'value',SystemInfo.version)
+set_param([modelname,'/SystemInfo/date'],'value',SystemInfo.date)
+save_system(modelname);
+pause(2);
+%% 编译
 rtwbuild(modelname)
+%% 保存系统信息
+save(infofilename,'SystemInfo')
