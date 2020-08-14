c58287f112a6e4c5a009b430e7f1f285b40e3e38
sl <sl1986-2-23@163.com>
2020-08-14 12:40:38 +0800

    1. 降落模式，低高度判定阈值由2m改为5m； 2. 增加盘旋下降模式高度过低应急返航，检测到高度低于切换高度20m持续0.3s后，将切换到旋翼然后返航着陆；

--- 任务模型/ENUM_RTInfo_Task.m
+++ 任务模型/ENUM_RTInfo_Task.m
@@ -161,6 +161,8 @@ classdef ENUM_RTInfo_Task < Simulink.IntEnumType
         VerticalMove_BeforePath_Enter_No (256),
         LargeAttitudeDetect_Enter_Normal (257),
         LargeAttitudeDetect_Enter_Abnormal (258),
+        TaskLog_Protect_HeightInHoverUp_Normal (259),
+        TaskLog_Protect_HeightInHoverUp_TooLow (260),
         
         ReciveCmd_GoHome (1201),
         ReciveCmd_GoHome_But_Not_Response (1202),
