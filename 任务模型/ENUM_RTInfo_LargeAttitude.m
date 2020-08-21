4149c41b5cfa0941d344fd9aa9f88ca8794a49e8
sl <sl1986-2-23@163.com>
2020-08-21 16:22:52 +0800

    1）修改turnCenterLL的维数定义 2）将遗漏文件加入工程

---
+++ 任务模型/ENUM_RTInfo_LargeAttitude.m
@@ -0,0 +1,9 @@
+classdef ENUM_RTInfo_LargeAttitude < Simulink.IntEnumType
+    %UNTITLED 此处显示有关此类的摘要
+    %   此处显示详细说明
+    enumeration
+        LargeAttitude_Normal (0),
+        LargeAttitude_Abnormal (1),
+    end
+end
+
---
+++ 飞控顶层架构模型/ENUM_UavModel_Architecture.m
@@ -0,0 +1,9 @@
+classdef ENUM_UavModel_Architecture < Simulink.IntEnumType
+    %UNTITLED 此处显示有关此类的摘要
+    enumeration
+        V1000_Architecture (0),
+        V10_Architecture (1),
+        V1000_V10_Architecture (2),
+    end
+end
+
