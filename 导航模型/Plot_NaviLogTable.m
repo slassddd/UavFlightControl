0aefac5a71ccf6cb4806a8ac47734eb34d50b294
slassddd <33631058+slassddd@users.noreply.github.com>
2020-12-11 15:26:07 +0800

    1）导航模块深度整理：增加事件log；更改输入变量处理方式； 2）完善着陆模式中对radar高度跳变的处理逻辑，提高安全性；

---
+++ 导航模型/Plot_NaviLogTable.m
@@ -0,0 +1,12 @@
+
+naviLogDataRes(1).time_sec = out.naviLogData1.time_sec.Data;
+naviLogDataRes(1).idx = out.naviLogData1.idx.Data;
+naviLogDataRes(1).blockName = out.naviLogData1.blockName.Data;
+naviLogDataRes(1).message = out.naviLogData1.message.Data;
+naviLogDataRes(1).var1 = out.naviLogData1.var1.Data;
+%%
+T_naviLog_All = parserNaviLogData(naviLogDataRes)
+matchMessages = ENUM_NaviLogBlockName.NAVILOG_Marg22;
+T_naviLog_Marg22 = parserNaviLogData(naviLogDataRes,matchMessages);
+matchMessages = ENUM_NaviLogBlockName.NAVILOG_Init;
+T_naviLog_Init = parserNaviLogData(naviLogDataRes,matchMessages);
