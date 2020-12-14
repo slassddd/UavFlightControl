1c03fac4b62f58a8ea2e193e82a80384e8090844
slassddd <33631058+slassddd@users.noreply.github.com>
2020-12-14 09:38:00 +0800

    1）导航模块深度整理： 增加事件log； 更改输入变量处理方式； 去掉ECAS输出； 整理输出, 完成结果对比； 完成NAVI2FIRM的替换，需跑全流程仿真进行验证； 2）完善着陆模式中对radar高度跳变的处理逻辑，提高安全性

--- 导航模型/ENUM_RTInfo_Navi.m
+++ 导航模型/ENUM_RTInfo_Navi.m
@@ -2,10 +2,16 @@ classdef ENUM_RTInfo_Navi < Simulink.IntEnumType
     %UNTITLED 此处显示有关此类的摘要
     %   此处显示详细说明
     enumeration
-        HelloWorld (0),
-        InitialWait_1_Complete (101),
-        InitialWait_2_Complete (102),
-        Initial_Complete (103),
+        RTIN_None (0),
+        RTIN_InitWait_1_Complete (101),
+        RTIN_InitWait_2_Complete (102),
+        RTIN_Init_Complete (103),
+        RTIN_Init_Enter (104),
+        
+        RTIN_Filter_Fuse_Ublox (201),
+        RTIN_Filter_Fuse_Um482 (202),
+        RTIN_Filter_Fuse_Baro (203),
+        RTIN_Filter_Fuse_Mag (204),
     end
 end
 
--- 导航模型/parserNaviLogData.m
+++ 导航模型/parserNaviLogData.m
@@ -1,4 +1,5 @@
 function T = parserNaviLogData(logData,matchMessages)
+enableSetName = true;
 blockNames = enumeration('ENUM_NaviLogBlockName');
 if nargin == 1    
     allBlockName = true;
@@ -46,6 +47,7 @@ logDataAll.message = logDataAll.message(idxSort);
 logDataAll.var1 = logDataAll.var1(idxSort,:);
 % 同时刻按idx升序
 timeUnique = unique(logDataAll.time_sec);
+varname = cell(length(timeUnique),5);
 for i = 1:length(timeUnique)
     thisTime = timeUnique(i);
     idxSameTime = find(logDataAll.time_sec == thisTime);
@@ -59,8 +61,52 @@ for i = 1:length(timeUnique)
         logDataAll.blockName(idxSameTime) = logDataAll.blockName(idxSameTimeNew);
         logDataAll.message(idxSameTime) = logDataAll.message(idxSameTimeNew);
         logDataAll.var1(idxSameTime,:) = logDataAll.var1(idxSameTimeNew,:);
+        if enableSetName
+            message = logDataAll.message(idxSameTime);
+            varname(idxSameTime,:) = setVarName(message);
+        end
+    else
+        if enableSetName
+            message = logDataAll.message(idxSameTime);
+            varname(idxSameTime,:) = setVarName(message);
+        end        
     end
 end
 % 建立Table
-T = table(logDataAll.time_sec, logDataAll.idx, logDataAll.blockName, logDataAll.message, logDataAll.var1);
-T.Properties.VariableNames = {'记录时间','同时刻序号','模块位置','message','var1'};
+if ~enableSetName
+    T = table(logDataAll.time_sec, logDataAll.idx, logDataAll.blockName, logDataAll.message, logDataAll.var1);
+    T.Properties.VariableNames = {'记录时间','同时刻序号','模块位置','message','var1'};
+else
+    T = table(logDataAll.time_sec, logDataAll.idx, logDataAll.blockName, logDataAll.message, ...
+        varname(:,1),logDataAll.var1(:,1),...
+        varname(:,2),logDataAll.var1(:,2),...
+        varname(:,3),logDataAll.var1(:,3),...
+        varname(:,4),logDataAll.var1(:,4),...
+        varname(:,5),logDataAll.var1(:,5));
+    T.Properties.VariableNames = {'记录时间','同时刻序号','模块位置','message',...
+        'name1','var1',...
+        'name2','var2',...
+        'name3','var3',...
+        'name4','var4',...
+        'name5','var5'};    
+end
+
+%% 子函数
+function varname = setVarName(message)
+nMessage = length(message);
+varname = cell(nMessage,5);
+for i = 1:nMessage
+    thisMessage = message(i);
+    switch thisMessage
+        case ENUM_RTInfo_Navi.RTIN_Filter_Fuse_Ublox
+            varname(i,:) = {'更新周期','健康状态','pdop','星数','速度标准差'};
+        case ENUM_RTInfo_Navi.RTIN_Filter_Fuse_Um482
+            varname(i,:) = {'更新周期','健康状态','pdop','星数','BESTPOS'};
+        case ENUM_RTInfo_Navi.RTIN_Filter_Fuse_Mag
+            varname(i,:) = {'更新周期','设备号','健康1','健康2','地磁模值'};           
+        case ENUM_RTInfo_Navi.RTIN_Filter_Fuse_Baro
+            varname(i,:) = {'更新周期','健康状态','气压高','ublox高','um482高'};                
+        otherwise
+            varname(i,:) = {' ',' ',' ',' ',' '};
+    end
+end
