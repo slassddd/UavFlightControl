aa3573e169e4d10c369ffaad597d75aaeaa86ac7
slassddd <33631058+slassddd@users.noreply.github.com>
2020-12-17 15:33:39 +0800

    1）常规整理；

--- 任务模型/INIT_GROUNDSTATION.m
+++ 任务模型/INIT_GROUNDSTATION.m
@@ -7,7 +7,7 @@ STRUCT_mavlink_msg_id_command_long = Simulink.Bus.createMATLABStruct('mavlink_ms
 STRUCT_BUS_TASK_COMMON_OutParam = Simulink.Bus.createMATLABStruct('BUS_TASK_COMMON_OutParam');
 %% º½Ïß
 deg2m = 1/111e3;
-pathHeight = 1209;
+pathHeight = 209;
 homeHeight = pathHeight + 0*200;
 % TASK_SET.PATH.home = [40.04 116.367 homeHeight]; %  lat lon alt
 TASK_SET.PATH.home = [40.04 180 homeHeight]; %  lat lon alt
@@ -33,12 +33,12 @@ switch pathExmpale
         numLine = 0;
         lon_left = 1e3;
         lon_right = 2e3;
-        lat_space = 50;
+        lat_space = 150;
         %         lon_right = 1.5e3;
         %         lat_space = 50;
         TASK_SET.PATH.paths_m(1,:) = 0*[0*lat_space, 0.5*lon_left, pathHeight];
         TASK_SET.PATH.paths_m(1,3) = pathHeight;
-        nPoints = 155;
+        nPoints = 15;
         for i = 2:nPoints
             if rem(i,4) == 2
                 lon_pos = lon_left;
